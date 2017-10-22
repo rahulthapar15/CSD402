21809c2650bf0f9b4e18ff4bd97017a4bbecebe4
Rahul Thapar <rahthap@gmail.com>
2017-10-22 21:56:31 +0530

    Adding to DB

--- Quora/connect.js
+++ Quora/connect.js
@@ -3,6 +3,9 @@
 var category = document.getElementById("category");
 var submit = document.getElementById("submitBtn");
 
+var firebase_ref = firebase.database().ref();
 function submitClick(){
     // window.alert("Working");
+    firebase_ref.child("Text").set("Some Value");
+    window.alert("DONE");
 }
