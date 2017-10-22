7cebfca0b23629d79d424a4e8cebdbbdac19f967
Rahul Thapar <rahthap@gmail.com>
2017-10-22 22:58:06 +0530

    Category Panel

--- Quora/connect.js
+++ Quora/connect.js
@@ -3,9 +3,15 @@
 var category = document.getElementById("category");
 var submit = document.getElementById("submitBtn");
 
+var dropdown = document.getElementById("menu1");
+// var opt = document.createElement("option");
+
+
 var firebase_ref = firebase.database().ref();
 function submitClick(){
     // window.alert("Working");
-    firebase_ref.child("Text").set("Some Value");
+    var mCategory = category.value;
+    firebase_ref.child("Category").push().set(mCategory);
+
     window.alert("DONE");
 }
