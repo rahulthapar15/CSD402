ebeb14291fc0a91c1c26e6797c104eca2025e072
Rahul Thapar <rahthap@gmail.com>
2017-10-22 21:11:30 +0530

    Backend

---
+++ Quora/connect.js
@@ -0,0 +1,8 @@
+// window.alert("OKAY");
+
+var category = document.getElementById("category");
+var submit = document.getElementById("submitBtn");
+
+function submitClick(){
+    // window.alert("Working");
+}
