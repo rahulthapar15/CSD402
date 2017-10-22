8c4c899f42e66cd4fb06a1ce26e113dc3b6414ee
Rahul Thapar <rahthap@gmail.com>
2017-10-22 23:20:55 +0530

    Category Completed

--- Quora/connect.js
+++ Quora/connect.js
@@ -1,4 +1,16 @@
 // window.alert("OKAY");
+var firebase_ref = firebase.database().ref();
+
+
+    var root_ref = firebase.database().ref().child("Category");
+    root_ref.on("child_added", snap =>{
+
+
+        var cat_name = snap.val();
+        $("#panel_body").append(cat_name+", ");
+
+
+    });
 
 var category = document.getElementById("category");
 var submit = document.getElementById("submitBtn");
@@ -7,7 +19,7 @@ var dropdown = document.getElementById("menu1");
 // var opt = document.createElement("option");
 
 
-var firebase_ref = firebase.database().ref();
+
 function submitClick(){
     // window.alert("Working");
     var mCategory = category.value;
